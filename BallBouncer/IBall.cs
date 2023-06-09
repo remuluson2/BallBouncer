a56a738dde4a5bcee96b06aa2389f40bd7fc50a1
Mateusz Dargiel <remuluson2@gmail.com>
2023-06-09 10:54:45 +0200

    Dodaj pliki projektów.

---
+++ BallBouncer/IBall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallBouncer
+{
+    public interface IBall
+    {
+        Vector2 Pos { get; set; }
+        Vector2 Speed { get; set; }
+        int Size { get; set; }
+        int Mass { get; set; }
+    }
+}
---
+++ BallBouncer/IBallManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallBouncer
+{
+    public interface IBallManager
+    {
+        void AddBall();
+        void AddBall(IBall ball);
+        void AddBall(Vector2 position);
+        IBall GetBall(int index);
+        void ClearBalls();
+        void UpdateBalls();
+        void UpdateSize(int width, int height);
+    }
+}
